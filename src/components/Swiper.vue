4a8f5c772c2725230053215b480789d984e5256a
terry <472283504@qq.com>
2018-05-03 15:06:35 +0800

    new

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="wrapper">
   <swiper :options="swiperOption" class="swiper-box">
-    <swiper-slide v-for='item of swiperList'>
+    <swiper-slide v-for='(item, index) of swiperList' :key='index'>
       <img :src="item.imgUrl">
     </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
